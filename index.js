a7c97b81ac9c3a2298b37b20355eca29d0da82e8
unknown <jtjackson325@gmail.com>
2022-06-25 18:18:00 -0600

    just typed the inquirer questions for the prompts for the title and descripton parts of the readme file

--- index.js
+++ index.js
@@ -4,7 +4,27 @@ const inquirer = require('inquirer');
 const generateMarkddown = require('./Develop/utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [{
+    type: 'input',
+    name: 'title',
+    message: 'What is the title for your project'
+},
+{
+    type: 'input',
+    name: 'reason',
+    message: 'Why did you begin this project'
+},
+{
+    type: 'input',
+    name: 'purpose',
+    message: 'What is the purpose behind your project'
+},
+{
+    type: 'input',
+    name: 'goal',
+    message: 'What goals do you hope to reach with your project'
+}
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
