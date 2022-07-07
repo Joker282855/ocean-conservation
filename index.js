f4dfcdebbf96d13263f7daf98ac70f4f651cd4c2
unknown <jtjackson325@gmail.com>
2022-07-07 12:42:50 -0600

    typed the questions for the testing and contributing section of the readme file

--- index.js
+++ index.js
@@ -7,43 +7,58 @@ const generateMarkddown = require('./Develop/utils/generateMarkdown');
 const questions = [{
     type: 'input',
     name: 'title',
-    message: 'What is the title for your project'
+    message: 'What is the title for your project?'
 },
 {
     type: 'input',
     name: 'reason',
-    message: 'Why did you begin this project'
+    message: 'Why did you begin this project?'
 },
 {
     type: 'input',
     name: 'purpose',
-    message: 'What is the purpose behind your project'
+    message: 'What is the purpose behind your project?'
 },
 {
     type: 'input',
     name: 'goal',
-    message: 'What goals do you hope to reach with your project'
+    message: 'What goals do you hope to reach with your project?'
 },
 {
     type: 'input',
     name: 'intention',
-    message: 'How do you want other people to use your project'
+    message: 'How do you want other people to use your project?'
 },
 {
     type: 'input',
     name: 'application',
-    message: 'How do you want other poeple to use the application for the readme file'
+    message: 'How do you want other poeple to use the application for the readme file?'
 },
 {
     type: 'input',
     name: 'computer',
-    message: 'How does another person use this code on their own computer'
+    message: 'How does another person use this code on their own computer?'
 },
 {
     type: 'list',
     name: 'license',
     message: 'What license does your readme file generator use? (Choose from the following list)',
     choices: ['MIT', 'GNU', 'Apache', 'ISC']
+},
+{
+    type: 'input',
+    name: 'contribution',
+    message: 'How should others make contributions to your website?'
+},
+{
+    type: 'input',
+    name: 'application-tribute',
+    message: 'How should others share your readme application with others?'
+},
+{
+    type: 'input',
+    name: 'tested',
+    message: 'What tests did you do to make sure the readme file was generated correctly?'
 }
 ];
 
