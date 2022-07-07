150a11f6d4788f11033c73ef1baa01ac6c708cff
unknown <jtjackson325@gmail.com>
2022-07-07 12:51:36 -0600

    got the functions written to capture the answers from the prompts with the inquirer package

--- index.js
+++ index.js
@@ -63,10 +63,27 @@ const questions = [{
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+
+    fs.writeFile(fileName, data, function(err) {
+        console.log(fileNmae)
+        console.log(data)
+            if (err) {
+                return console.log(err)
+            } else {
+                console.log('Readme created')
+            }
+    })
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions)
+        .then(function(data) {
+            writeToFile('README.md', generateMarkddown(data));
+            console.log(data);
+        })
+}
 
 // Function call to initialize app
 init();
