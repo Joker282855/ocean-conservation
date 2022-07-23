18bbb200a1768126037667347bc652ada5e4d9ac
unknown <jtjackson325@gmail.com>
2022-07-23 14:16:49 -0600

    fixed some errors with the gtammar on the readme and moving between sections

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -19,11 +19,13 @@ function generateMarkdown(data) {
 
   # Description:
   ${data.reason}
+  ${data.purpose}
   ${data.intention}
 
   # Table of Contents:
   -[Installation](#installation)
   -[Usage](#usage)
+  -[License](#license)
   -[Contributing](#contributing)
   -[Tests](#tests)
   -[Questions](#questions)
@@ -48,11 +50,13 @@ function generateMarkdown(data) {
   # Tests
   ${data.tested}
 
-  # Works Shown
-  The link to my github profile is https://github.com/${data.profile}
+  # Questions
+  The link to my github profile is https://github.com/${data.profile}.
+  My email address is ${data.email}.
+  My phone number is ${data.phone}.
 
-  # Contact
-  I am usually busy working from 9:00 a,m. to 5:00 p.m. and reserve two hours after work to check if someone has reached out to me. The different ways a person can reach me will be through my email or phone number.
+  I usally work my professional job from 9:00 a.m. to 5:00 p.m. I would be free anytime after that if you need to contact me. I am happy to collaborate with anyone that wants to help me impprove my website and my code.
+  Learning from others Is what helps a person grow and become a better coder overall. I look forward to colloboarating with other people soon. 
 
 `;
 }
